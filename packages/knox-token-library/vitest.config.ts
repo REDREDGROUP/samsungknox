2a3202bd42577596264c2de7a4507f7a7c74d4ad
jieey1140 <seihoonkim@hansei.kr>
2023-10-27 12:18:02 +0900

    fix(vitest config): test option fix

--- packages/knox-token-library/vitest.config.ts
+++ packages/knox-token-library/vitest.config.ts
@@ -3,6 +3,10 @@ import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    coverage: {
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ['text', 'json-summary', 'json'],
+    },
     exclude: [...configDefaults.exclude],
   },
   resolve: {
