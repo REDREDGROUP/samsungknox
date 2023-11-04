53710f6e28576ec2040197cd39f118185243262a
jieey1140 <seihoonkim@hansei.kr>
2023-11-05 02:58:44 +0900

    fix(samsungknox-docs): docusaurus.config.ts fix

--- packages/knox-docs/docusaurus.config.ts
+++ packages/knox-docs/docusaurus.config.ts
@@ -8,14 +8,14 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://redredgroup.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/samsungknox/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'REDREDGROUP/samsungknox', // Usually your GitHub org/user name.
+  organizationName: 'REDREDGROUP', // Usually your GitHub org/user name.
   projectName: 'samsungknox', // Usually your repo name.
 
   onBrokenLinks: 'throw',
